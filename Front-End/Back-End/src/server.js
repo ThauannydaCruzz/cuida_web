13b28c775225d8dc83f54f93d1c658d47ea29e11
GabrielaAkemi <gabyhizukuri@gmail.com>
2025-11-04 18:41:41 -0300

    Mensagem que descreve as mudanças

--- Front-End/Back-End/src/server.js
+++
@@ -1,7 +0,0 @@
-// server.js
-require('dotenv').config({ path: __dirname + '.env' });
-
-const app = require("./app");
-
-const PORT = process.env.PORT_SERVER;
-app.listen(PORT, () => console.log(`🚀 Servidor rodando na porta ${PORT}`));
--- Front-End/Front-End/src/services/auth.ts
+++
@@ -1,5 +0,0 @@
-export const logout = () => {
-  // Lógica simples de logout (limpar dados de sessão se houver)
-  sessionStorage.clear();
-  localStorage.clear();
-};
